859edcfee5429ba3ce5e17b8cbf0e53a6e0d41c5
albertoatx <anugnaha@yahoo.es>
2018-01-10 20:43:33 +0100

    added feature4 file

---
+++ feature4.js
@@ -0,0 +1,5 @@
+console.log("feature4");
+console.log("Creada una branch nueva para este nuevo componente");
+//console.log("Luego hacemos commit");
+//console.log("Luego cambio en CSS y hacemos otro commit");
+//console.log("Esperamos");
